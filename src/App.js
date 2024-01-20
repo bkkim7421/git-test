728164f46e882ccde8769331a607c2f91d73fd04
bkkim7421 <bkkim0421@naver.com>
2024-01-21 00:28:47 +0900

    버어튼

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import Button from "./Button";
+
 function App() {
+    const [value, setValue] = React.useState("OwO");
+
+    const changeValue = () => {
+        setValue("Yeah~");
+    };
+
     return (
         <div>
             <h1>God Damn!</h1>
+            <Button text={value} fontSize={18} onClick={changeValue} />
         </div>
     );
 }
